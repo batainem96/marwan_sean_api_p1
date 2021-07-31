bf54bc9a6b6d71a90cc9cf76a9d207e5a40d0396
Sean <seanprdunn@gmail.com>
2021-07-31 15:21:38 -0400

    Initial push

---
+++ src/main/java/com/revature/schoolDatabase/util/ProgramManager.java
@@ -0,0 +1,13 @@
+package com.revature.schoolDatabase.util;
+
+public class ProgramManager {
+
+    public ProgramManager() {
+
+    }
+
+    public void start() {
+        System.out.println("Starting now");
+    }
+
+}
