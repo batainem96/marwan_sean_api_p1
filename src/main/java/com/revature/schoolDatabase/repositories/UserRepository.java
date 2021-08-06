0eff2fc37bceae45ef8f6ade1ae96ca2272d209d
Sean <seanprdunn@gmail.com>
2021-08-06 17:50:27 -0400

    Ported UserRepository.findUserByCredentials from bookstore to p0

--- src/main/java/com/revature/schoolDatabase/repositories/UserRepository.java
+++ src/main/java/com/revature/schoolDatabase/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.revature.schoolDatabase.repositories;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -10,6 +12,32 @@ import org.bson.Document;
 
 public class UserRepository implements CrudRepository<Person> {
 
+    public Person findUserByCredentials(String username, String password) {
+        try {
+            MongoClient mongoClient = MongoClientFactory.getInstance().getConnection();
+            MongoDatabase bookstoreDatabase = mongoClient.getDatabase("bookstore");
+            MongoCollection<Document> usersCollection = bookstoreDatabase.getCollection("users");
+            Document queryDoc = new Document("username", username).append("password", password);
+            Document authUserDoc = usersCollection.find(queryDoc).first();
+
+            if (authUserDoc == null) {
+                return null;
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            Person authUser = mapper.readValue(authUserDoc.toJson(), Person.class);
+            authUser.setId(authUserDoc.get("_id").toString());
+            return authUser;
+
+        } catch (JsonMappingException jme) {
+            jme.printStackTrace(); // TODO log this to a file
+            throw new DataSourceException("An exception occurred while mapping the document.", jme);
+        } catch (Exception e) {
+            e.printStackTrace(); // TODO log this to a file
+            throw new DataSourceException("An unexpected exception occurred.", e);
+        }
+
+    }
 
     @Override
     public Person findById(int id) {
