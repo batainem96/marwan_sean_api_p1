55d6a9d25bb91b23c8e32923f27722600654e26c
Sean <seanprdunn@gmail.com>
2021-08-16 20:24:05 -0400

    Fixed ContextLoader issue and merged with Refactoring branch

--- src/main/java/com/revature/schoolDatabase/util/exceptions/ResourcePersistenceException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.schoolDatabase.util.exceptions;
-
-public class ResourcePersistenceException extends RuntimeException {
-
-    public ResourcePersistenceException(String message) {
-        super(message);
-    }
-
-}
