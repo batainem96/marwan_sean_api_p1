eda39c293fb34dd51d721543133062753570539d
Marwan <batainem96@gmail.com>
2021-08-18 22:26:31 -0500

    Added HealthCheckServlet (wasn't included in previous push for some reason)

---
+++ src/main/java/com/revature/schoolDatabase/web/servlets/HealthCheckServlet.java
@@ -0,0 +1,16 @@
+package com.revature.schoolDatabase.web.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HealthCheckServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("{ \"status\": \"UP\" }");
+    }
+
+}
